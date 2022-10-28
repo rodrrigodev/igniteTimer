ea828ecc1761deb07f5ba6fe84d2992c092070af
rodrrigodev <rodrigosanyy@gmail.com>
2022-10-28 09:33:08 -0300

    header and layout add

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import { DefaultLayout } from "./layouts/DefaultLayout"
+import { DefaultLayout } from "./layouts/DefaulLayout"
 import { History } from "./pages/History"
 import { Home } from "./pages/Home"
 
