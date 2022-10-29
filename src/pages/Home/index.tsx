489516d7eb676cc7dd8f61f97f920f5c20fbd2be
rodrrigodev <rodrigosanyy@gmail.com>
2022-10-29 11:24:03 -0300

    some styles add

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,24 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+                    <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1"/>
+                        <option value="Projeto 2"/>
+                        <option value="Projeto 3"/>
+                        <option value="Manga"/>
+                    </datalist>
 
                     <label htmlFor="minutesAmout">Durante</label>
-                    <MinutesAmountInput type="number" id="minutesAmout" placeholder="00" />
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmout"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -24,7 +38,7 @@ export function Home() {
                 </CountdownContainer>
 
                 <StartCountDownButton disabled type="submit">
-                    <Play size={24}/> Começar
+                    <Play size={24} /> Começar
                 </StartCountDownButton>
             </form>
         </HomeContainer>
